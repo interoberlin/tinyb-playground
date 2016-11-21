07dba4ce586ad85e00a612acb4219a810d7e49f6
Florian Schwanz <florian.schwanz@interoberlin.de>
2016-11-21 02:14:36 +0100

    Restructured code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,32 @@
 import de.interoberlin.tinyb_playground.EAction
 import de.interoberlin.tinyb_playground.TinybController
-import tinyb.BluetoothManager
+import tinyb.BluetoothDevice
+import tinyb.BluetoothGattCharacteristic
 
 fun main(args: Array<String>) {
     val tinybController = TinybController.getInstance()
-    val manager = BluetoothManager.getBluetoothManager()
 
-    // Scan devices
-
-    val devices = tinybController.scanDevices(manager)
-
-    tinybController.showDevices(devices)
-
-    val device = tinybController.selectDevice(devices)
+    var devices: List<BluetoothDevice>? = null
+    var device: BluetoothDevice? = null
+    var characteristic: BluetoothGattCharacteristic? = null
 
     do {
-        var action = tinybController.selectAction()
+        val action = tinybController.selectAction()
         when (action) {
-            EAction.CONNECT -> tinybController.connectDevice(device)
-            EAction.DISCONNECT -> tinybController.disconnectDevice(device)
-            EAction.PAIR -> tinybController.pairDevice(device)
-            EAction.FIND_SERVICE -> tinybController.findService(device)
-            EAction.SHOW_SERVICES -> tinybController.showServices(device)
-            EAction.READ_CHARACTERISTIC -> tinybController.readCharacteristic(device);
-            EAction.WRITE_CHARACTERISTIC -> tinybController.writeCharacteristic(device);
+            EAction.HELP -> tinybController.showHelp()
+            EAction.SCAN -> devices = tinybController.scanDevices()
+            EAction.SELECT_DEVICE -> if (devices != null) device = tinybController.selectDevice(devices)
+            EAction.CONNECT -> if (device != null) tinybController.connectDevice(device)
+            EAction.DISCONNECT -> if (device != null) tinybController.disconnectDevice(device)
+            EAction.PAIR -> if (device != null) tinybController.pairDevice(device)
+            EAction.FIND_SERVICE -> if (device != null) tinybController.findService(device)
+            EAction.SHOW_SERVICES -> if (device != null) tinybController.showServices(device)
+            EAction.READ_CHARACTERISTIC -> if (device != null) tinybController.readCharacteristic(device)
+            EAction.WRITE_CHARACTERISTIC -> if (device != null) tinybController.writeCharacteristic(device)
+            EAction.SUBSCRIBE_CHARACTERISTIC -> if (device != null) characteristic = tinybController.subscribeCharacteristic(device)
+            EAction.UNSUBSCRIBE_CHARACTERISTIC -> if (device != null && characteristic != null) tinybController.unsubscribeCharacteristic(device, characteristic)
             EAction.QUIT -> println("Byebye")
+            else -> println("Command not found. Type 'help' to show the help page or type 'quit' to end this program.")
         }
-    } while (action != EAction.QUIT);
+    } while (action != EAction.QUIT)
 }
--- src/main/kotlin/de/interoberlin/tinyb_playground/TinybController.kt
+++ src/main/kotlin/de/interoberlin/tinyb_playground/TinybController.kt
@@ -2,218 +2,33 @@ package de.interoberlin.tinyb_playground
 
 import parser.BleDataParser
 import repository.ECharacteristic
-import tinyb.BluetoothDevice
-import tinyb.BluetoothException
-import tinyb.BluetoothGattService
-import tinyb.BluetoothManager
+import tinyb.*
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 class TinybController
-private constructor() {
-
+private constructor() : BluetoothNotification<ByteArray> {
     // --------------------
-    // Methods
+    // Companion object
     // --------------------
 
-
-    @Throws(InterruptedException::class)
-    fun scanDevices(manager: BluetoothManager): List<BluetoothDevice> {
-        manager.startDiscovery()
-        println("Start scan")
-
-        for (i in 0..SCAN_DURATION - 1) {
-            print(".")
-            Thread.sleep(1000)
-        }
-
-        try {
-            manager.stopDiscovery()
-        } catch (e: BluetoothException) {
-            System.err.println("Discovery could not be stopped.")
-        }
-
-        println("")
-        return manager.devices
-    }
-
-    fun selectDevice(devices: List<BluetoothDevice>): BluetoothDevice {
-        println("\nSelect a device")
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        var pickedDeviceID: Int
-
-        while (true) {
-            try {
-                pickedDeviceID = Integer.parseInt(br.readLine())
-            } catch (e: NumberFormatException) {
-                println(".. not a number")
-                continue
-            }
-
-            if (pickedDeviceID > devices.size) {
-                println(".. not in range")
-                continue
-            }
-
-            return devices[pickedDeviceID - 1]
-        }
-    }
-
-    fun selectAction(): EAction {
-        println("\nSelect an action")
-        println("  [C]onnect")
-        println("  [D]isconnect")
-        println("  [P]air")
-        println("  [F]ind service")
-        println("  [S]how services")
-        println("  [R]ead characteristic")
-        println("  [W]rite characteristic")
-        println("  [Q]uit")
-        val br = BufferedReader(InputStreamReader(System.`in`))
-
-        while (true) {
-            val input = br.readLine().toLowerCase().substring(0, 1)
-
-            when (input) {
-                "c" -> return EAction.CONNECT
-                "d" -> return EAction.DISCONNECT
-                "p" -> return EAction.PAIR
-                "f" -> return EAction.FIND_SERVICE
-                "s" -> return EAction.SHOW_SERVICES
-                "r" -> return EAction.READ_CHARACTERISTIC
-                "w" -> return EAction.WRITE_CHARACTERISTIC
-                "q" -> return EAction.QUIT
-            }
-        }
-    }
-
-    fun connectDevice(device: BluetoothDevice) {
-        print("\nConnect device " + ANSI_GREEN + device.address + ANSI_RESET + " " + device.name)
-        if (device.connect()) {
-            println(".. sensor with the provided address connected")
-            println("paired  : " + device.paired)
-            println("trusted : " + device.trusted)
-        } else {
-            println(".. could not connect device")
-            System.exit(-1)
-        }
-    }
-
-    fun disconnectDevice(device: BluetoothDevice) {
-        print("\n" + ANSI_RED + "Dis" + ANSI_RESET + "connect device " + ANSI_GREEN + device.address + ANSI_RESET + " " + device.name)
-        if (device.disconnect())
-            println(".. sensor with the provided address " + ANSI_RED + "dis" + ANSI_RESET + "connected")
-        else {
-            println(".. could not disconnect device")
-            System.exit(-1)
-        }
-    }
-
-    fun pairDevice(device: BluetoothDevice) {
-        device.pair()
-    }
-
-    fun findService(device: BluetoothDevice) {
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        val input = br.readLine()
-
-        device.find(input)
-    }
-
-    fun showDevices(devices: List<BluetoothDevice>) {
-        var i = 0
-        for (device in devices) {
-            if (device.connected) {
-                println(ANSI_GREEN + "#" + ++i + ANSI_RESET + " " + device.address + " " + device.name + " " + device.connected)
-            } else {
-                println(ANSI_RED + "#" + ++i + ANSI_RESET + " " + device.address + " " + device.name + " " + device.connected)
-            }
-        }
-    }
-
-    fun showServices(device: BluetoothDevice) {
-        println("Wait for services")
-
-        var bluetoothServices: List<BluetoothGattService>
-
-        for (i in 1..10) {
-            bluetoothServices = device.services
-
-            if (bluetoothServices.isEmpty()) {
-                print(".")
-                Thread.sleep(1000)
-            } else {
-                for (service in bluetoothServices) {
-                    println("Service " + service.uuid)
-                    for (characteristic in service.characteristics) {
-                        println("Characteristic " + ANSI_PURPLE + characteristic.uuid + ANSI_RESET)
-                    }
-                }
-
-                break;
-            }
-        }
-    }
-
-    fun readCharacteristic(device: BluetoothDevice) {
-        println("Select characteristic to read")
-
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        val input = br.readLine()
-
-        for (service in device.services) {
-            for (characteristic in service.characteristics) {
-                if (input == characteristic.uuid) {
-                    val batteryLevel = BleDataParser.getFormattedValue(null, ECharacteristic.BATTERY_LEVEL, characteristic.readValue())
-                    print("Characteristic ${characteristic.uuid} : ${ANSI_YELLOW}${batteryLevel}${ANSI_RESET}")
-                    return
-                }
-            }
-        }
-
-        println("Characteristic with UUID ${input} not found")
-    }
-
-    fun writeCharacteristic(device: BluetoothDevice) {
-        println("Select characteristic to write")
-
-        val br = BufferedReader(InputStreamReader(System.`in`))
-        val input = br.readLine()
-
-        for (service in device.services) {
-            for (characteristic in service.characteristics) {
-                if (input == characteristic.uuid) {
-
-                    println(String.format("%8s", Integer.toBinaryString(byteArrayOf(0x01)[0].toInt()).replace(' ', '0')))
-                    println(String.format("%8s", Integer.toBinaryString(byteArrayOf(1.toByte())[0].toInt()).replace(' ', '0')))
-
-                    if (!device.connected) device.connect()
-                    val success = characteristic.writeValue(byteArrayOf(0x01))
-
-                    if (!device.connected) device.connect()
-                    val s = characteristic.writeValue(byteArrayOf(1.toByte()))
-
-                    println(if (success) "Characteristic ${characteristic.uuid} :${ANSI_YELLOW}0x01${ANSI_RESET}" else "Write failed")
-                    return
-                }
-            }
-        }
-
-        println("Characteristic with UUID ${input} not found")
-    }
+    // <editor-fold defaultstate="collapsed" desc="Companion object">
 
     companion object {
         private val SCAN_DURATION = 5
 
         private val ANSI_RESET = "\u001B[0m"
-        // private static final String ANSI_BLACK = "\u001B[30m";
         private val ANSI_RED = "\u001B[31m"
         private val ANSI_GREEN = "\u001B[32m"
-        private val ANSI_YELLOW = "\u001B[33m";
-        // private static final String ANSI_BLUE = "\u001B[34m";
+        private val ANSI_YELLOW = "\u001B[33m"
         private val ANSI_PURPLE = "\u001B[35m"
-        // private static final String ANSI_CYAN = "\u001B[36m";
-        // private static final String ANSI_WHITE = "\u001B[37m";
+        private val ANSI_CYAN = "\u001B[36m"
+
+        private val DEBUG = "${ANSI_CYAN}DEBUG$ANSI_RESET"
+        private val INFO = "$ANSI_GREEN INFO$ANSI_RESET"
+        // private val WARN = "$ANSI_YELLOW WARN$ANSI_RESET"
+        private val ERROR = "${ANSI_RED}ERROR$ANSI_RESET"
+        private val INPUT = "${ANSI_PURPLE}INPUT$ANSI_RESET"
 
         private var inst: TinybController? = null
 
@@ -225,4 +40,257 @@ private constructor() {
             return inst as TinybController
         }
     }
+
+    // </editor-fold>
+
+    // --------------------
+    // Methods
+    // --------------------
+
+    // <editor-fold defaultstate="collapsed" desc="Methods">
+
+    fun selectAction(): EAction {
+        while (true) {
+            val input = BufferedReader(InputStreamReader(System.`in`)).readLine().toLowerCase()
+
+            if (input.length < 3) return EAction.RETRY
+
+            when (input.substring(0, 3)) {
+                "hel" -> return EAction.HELP
+                "sca" -> return EAction.SCAN
+                "sel" -> return EAction.SELECT_DEVICE
+                "con" -> return EAction.CONNECT
+                "dis" -> return EAction.DISCONNECT
+                "pai" -> return EAction.PAIR
+                "fin" -> return EAction.FIND_SERVICE
+                "sho" -> return EAction.SHOW_SERVICES
+                "rea" -> return EAction.READ_CHARACTERISTIC
+                "wri" -> return EAction.WRITE_CHARACTERISTIC
+                "sub" -> return EAction.SUBSCRIBE_CHARACTERISTIC
+                "uns" -> return EAction.UNSUBSCRIBE_CHARACTERISTIC
+                "qui" -> return EAction.QUIT
+                else -> return EAction.RETRY
+            }
+        }
+    }
+
+    fun showHelp() {
+        println("  [Hel]p")
+        println("  [Sca]n device")
+        println("  [Sel]ect device")
+        println("  [Con]nect")
+        println("  [Dis]connect")
+        println("  [Pai]r")
+        println("  [Fin]d service")
+        println("  [Sho]w services")
+        println("  [Rea]d characteristic")
+        println("  [Wri]te characteristic")
+        println("  [Sub]scribe characteristic")
+        println("  [Uns]ubscribe characteristic")
+        println("  [Q]uit")
+    }
+
+    @Throws(InterruptedException::class)
+    fun scanDevices(): List<BluetoothDevice> {
+        val manager = BluetoothManager.getBluetoothManager()
+        manager.startDiscovery()
+        println("$INFO Start scan")
+        print("      ")
+
+        for (i in 0..SCAN_DURATION - 1) {
+            print(".")
+            Thread.sleep(1000)
+        }
+
+        try {
+            manager.stopDiscovery()
+        } catch (e: BluetoothException) {
+            println("$ERROR Discovery could not be stopped.")
+        }
+        println("")
+
+        showDevices(manager.devices)
+        return manager.devices
+    }
+
+    fun showDevices(devices: List<BluetoothDevice>) {
+        println("$INFO Show devices")
+        var i = 0
+        for (device in devices) {
+            println("      " + if (device.connected) ANSI_GREEN else ANSI_RED + "# ${++i} $ANSI_RESET ${device.address} ${device.name} ${device.connected}")
+        }
+    }
+
+    fun selectDevice(devices: List<BluetoothDevice>): BluetoothDevice {
+        println("$INFO Select a device")
+
+        val input = BufferedReader(InputStreamReader(System.`in`)).readLine()
+        var pickedDeviceID: Int
+
+        while (true) {
+            try {
+                pickedDeviceID = Integer.parseInt(input)
+            } catch (e: NumberFormatException) {
+                println("$ERROR '$input' is not a number")
+                continue
+            }
+
+            if (pickedDeviceID > devices.size) {
+                println("$ERROR '$pickedDeviceID' is not in range")
+                continue
+            }
+
+            return devices[pickedDeviceID - 1]
+        }
+    }
+
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Methods connection">
+
+    fun connectDevice(device: BluetoothDevice) {
+        println("$INFO Connect device ${device.address} ${device.name}")
+        if (device.connect()) {
+            println("$DEBUG Paired : ${device.paired}")
+            println("$DEBUG Trusted: ${device.trusted}")
+        } else {
+            println("$ERROR Could not connect device")
+            System.exit(-1)
+        }
+    }
+
+    fun disconnectDevice(device: BluetoothDevice) {
+        println("$INFO Disconnect device ${device.address} ${device.name}")
+        if (!device.disconnect()) {
+            println("$ERROR Could not disconnect device")
+        }
+    }
+
+    fun pairDevice(device: BluetoothDevice) {
+        println("$INFO Pair device ${device.address} ${device.name}")
+        device.pair()
+    }
+
+    fun findService(device: BluetoothDevice) {
+        println("$INFO Find service")
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val input = br.readLine()
+
+        device.find(input)
+    }
+
+    fun showServices(device: BluetoothDevice) {
+        println("$INFO Show service")
+
+        var bluetoothServices: List<BluetoothGattService>
+
+        for (i in 1..10) {
+            if (!device.connected) device.connect()
+            bluetoothServices = device.services
+
+            for (service in bluetoothServices) {
+                println("     Service " + service.uuid)
+                for (characteristic in service.characteristics) {
+                    println("      Characteristic " + ANSI_CYAN + characteristic.uuid + ANSI_RESET)
+                }
+            }
+        }
+    }
+
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Methods communication">
+
+    fun readCharacteristic(device: BluetoothDevice) {
+        println("$INPUT Select characteristic to read")
+
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val input = br.readLine()
+
+        for (service in device.services) {
+            for (characteristic in service.characteristics) {
+                if (input == characteristic.uuid) {
+                    if (!device.connected) device.connect()
+                    val batteryLevel = BleDataParser.getFormattedValue(null, ECharacteristic.BATTERY_LEVEL, characteristic.readValue())
+                    print("Characteristic $characteristic.uuid : $ANSI_YELLOW$batteryLevel$ANSI_RESET")
+                    return
+                }
+            }
+        }
+
+        println("$ERROR Characteristic with UUID $input not found")
+    }
+
+    fun writeCharacteristic(device: BluetoothDevice) {
+        println("$INPUT Select characteristic to write")
+
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val input = br.readLine()
+
+        for (service in device.services) {
+            for (characteristic in service.characteristics) {
+                if (characteristic.uuid == input || characteristic.uuid.contains(input)) {
+                    if (!device.connected) device.connect()
+                    val success = characteristic.writeValue(byteArrayOf(0x01))
+
+                    println(if (success) "Characteristic ${characteristic.uuid} :${ANSI_YELLOW}0x01$ANSI_RESET" else "Write failed")
+                    return
+                }
+            }
+        }
+
+        println("$ERROR Characteristic with UUID $input not found")
+    }
+
+    fun subscribeCharacteristic(device: BluetoothDevice): BluetoothGattCharacteristic? {
+        println("$INPUT Select characteristic to subscribe")
+
+        val br = BufferedReader(InputStreamReader(System.`in`))
+        val input = br.readLine()
+
+        for (service in device.services) {
+            for (characteristic in service.characteristics) {
+                if (input == characteristic.uuid) {
+                    if (characteristic.uuid == input || characteristic.uuid.contains(input)) {
+                        if (!device.connected) device.connect()
+                        characteristic.enableValueNotifications(this)
+                        device.enableConnectedNotifications({
+                            println("$INFO Connected")
+                        })
+
+                        println("$INFO Characteristic ${characteristic.uuid} subscribed")
+                        return characteristic
+                    }
+                }
+            }
+        }
+
+        println("$ERROR Characteristic with UUID $input not found")
+        return null
+    }
+
+    fun unsubscribeCharacteristic(device: BluetoothDevice, characteristic: BluetoothGattCharacteristic) {
+        for (s in device.services) {
+            for (c in s.characteristics) {
+                if (c.uuid == characteristic.uuid) {
+                    characteristic.disableValueNotifications()
+                    println("$INPUT Characteristic ${characteristic.uuid} ${ANSI_YELLOW}unsubscribed$ANSI_RESET")
+                }
+            }
+        }
+    }
+
+    // </editor-fold>
+
+    // --------------------
+    // Callback interfaces
+    // --------------------
+
+    // <editor-fold defaultstate="collapsed" desc="Callback interfaces">
+
+    override fun run(value: ByteArray?) {
+        println("$INFO PING")
+    }
+
+// </editor-fold>
 }
